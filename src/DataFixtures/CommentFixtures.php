56d41c9920105ae7ca8aa5ab77c2d5fe4bc5a6eb
Weronika Podgórska <weronika.podgorska96@gmail.com>
2021-09-10 00:07:49 +0200

    comment section p.1

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -1,1 +1,48 @@
 <?php
+/**
+ * Book fixtures.
+ */
+
+namespace App\DataFixtures;
+
+use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * Class BookFixtures.
+ */
+class CommentFixtures extends AbstractBaseFixtures implements DependentFixtureInterface
+{
+    /**
+     * Load data.
+     *
+     * @param \Doctrine\Persistence\ObjectManager $manager Persistence object manager
+     */
+    public function loadData(ObjectManager $manager): void
+    {
+        $this->createMany(50, 'comment', function($i) {
+            $comment = new Comment();
+            $comment->setCreatedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
+            $comment->setUpdatedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
+            $comment->setContent($this->faker->sentence);
+            $comment->setUser($this->getRandomReference('users'));
+            $comment->setBook($this->getRandomReference('book'));
+
+            return $comment;
+        });
+
+        $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on.
+     *
+     * @return array Array of dependencies
+     */
+    public function getDependencies(): array
+    {
+        return [UserFixtures::class];
+    }
+}
