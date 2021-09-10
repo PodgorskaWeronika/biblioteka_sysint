39f475ea2c09461876eda423ff6f4eeaad1ba45d
Weronika Podgórska <weronika.podgorska96@gmail.com>
2021-09-10 17:42:33 +0200

    translations updated, book show & index templates updated

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -23,10 +23,6 @@ class CategoryRepository extends ServiceEntityRepository
     /**
      * Items per page.
      *
-     * Use constants to define configuration options that rarely change instead
-     * of specifying them in app/config/config.yml.
-     * See https://symfony.com/doc/current/best_practices.html#configuration
-     *
      * @constant int
      */
     const PAGINATOR_ITEMS_PER_PAGE = 10;
