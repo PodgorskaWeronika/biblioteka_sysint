9e1db400a2dcb272b663be5078f0aeae4cc07072
Weronika Podgórska <weronika.podgorska96@gmail.com>
2021-09-13 19:21:53 +0200

    exam version2

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\WebProfilerBundle;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
